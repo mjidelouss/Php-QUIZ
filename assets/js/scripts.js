675c5afda166a9856d7fede9aa979bfa946911d1
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2022-12-24 19:09:34 +0100

    optimized code and added comments to better explain it

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,6 +15,9 @@ let score = 0;
 let questionIndex = 0;
 let questionCounter = 0;
 let countdown = 10;
+let correctQuestionsCounter = 0;
+let incorrectQuestionsCounter = 0;
+const MAX_QUESTIONS = questions.length;
 
 // function shuffleArray to shuffle the array
 function shuffleArray(array) {
@@ -23,7 +26,9 @@ function shuffleArray(array) {
 
 const shuffledQuestions = shuffleArray(questions);
 
+// function initTest sets the data from the array
 function initTest() {
+    // Setting the Questions from the array
     question.innerText = shuffledQuestions[questionIndex].question;
     option1.innerText = shuffledQuestions[questionIndex].option1;
     option2.innerText = shuffledQuestions[questionIndex].option2;
@@ -40,10 +45,8 @@ function initTest() {
 
 let intervalId;
 
+// Function startCountdown starts the countdown
 function startCountdown() {
-  // Check if a countdown is already in progress
-  if (intervalId) return;
-
   // Display the countdown in the page
   timer.innerText = countdown;
 
@@ -51,29 +54,39 @@ function startCountdown() {
   intervalId = setInterval(() => {
     countdown--;
     timer.innerText = countdown;
+    // In case the countdown reaches 0
     if (countdown <= 0) {
       clearInterval(intervalId);
       intervalId = null;
+      // Incrementing the incorrect questions counter
+      incorrectQuestionsCounter++;
       // Move on to the next question after the countdown ends
       goToNextQuestion();
     }
   }, 1000);
 }
 
+// Function goToNextQuestion loads the next question
 function goToNextQuestion() {
-  if (questionCounter >= 10) 
+  questionCounter++;
+
+  // In case the questions reaches the maximum
+  if (questionCounter >= MAX_QUESTIONS) 
   {
-    // Display the final score to the user
+    // Setting values in the storage to be used in another page
     sessionStorage.setItem("score", score);
+    sessionStorage.setItem("correct", correctQuestionsCounter);
+    sessionStorage.setItem("incorrect", incorrectQuestionsCounter);
+    // Sending the user to the score.html page
     window.location.href = "score.html";
     return;
   }
   countdown = 10;
   // Clear the countdown from the page
   timer.innerText = '';
-  questionCounter++;
-  progressBarFull.style.width = `${(questionCounter / 10) * 100}%`;
-  progressText.innerText = `Question ${questionCounter} / ${10}`;
+  // Updating the Prograss Bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+  progressText.innerText = `Question ${questionCounter} / ${MAX_QUESTIONS}`;
   // Increment the question index and update the quiz with the new question
   questionIndex++;
   initTest();
@@ -84,23 +97,40 @@ options.forEach(option => {
         // Clear the countdown timer
         clearInterval(intervalId);
         intervalId = null;
+        // Selected option by the user
         const selectedoption = e.target;
         const correct = selectedoption.dataset.correct;
-        console.log(option.dataset.correct)
-        questionCounter++;
-        progressBarFull.style.width = `${(questionCounter / 10) * 100}%`;
-        progressText.innerText = `Question ${questionCounter} / ${10}`;
+
+        // Updating the progress bar when the user clicks on an option
+        progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+        progressText.innerText = `Question ${questionCounter} / ${MAX_QUESTIONS}`;
+
+        // In case the option is correct
         if (correct === 'true') {
+
+            // Incrementing the score and the correct Questions Counter 
             score += 100;
+            correctQuestionsCounter++;
             scoreText.innerText = score;
+
+            // adding the class correct to the selected option
             selectedoption.parentElement.classList.add('correct');
+            // adding the class incorrect to the rest of the options
             options.forEach (option => {
                 if (option != selectedoption) {
                     option.parentElement.classList.add('incorrect');
                 }
             })
+        
+        // In case the option is incorrect
         } else if (correct === 'false') {
+
+            // Incrementing the Incorrect Questions Counter
+            incorrectQuestionsCounter++;
+
+            // Adding the class incorrect to the selected option
             selectedoption.parentElement.classList.add('incorrect');
+            // Adding the correct class to the correct option and incorrect class to the other ones
             options.forEach(option => {
                 if (option !== selectedoption) {
                     if (option.dataset.correct === 'true') {
@@ -112,16 +142,17 @@ options.forEach(option => {
                 }
             });
         }
+        // Delaying of 1 second after the user clicks on an option
         setTimeout(() => {
+            // Removing the correct and incorrect classes from each option
             options.forEach(option => {
                 option.parentElement.classList.remove('correct');
                 option.parentElement.classList.remove('incorrect');
             });
             goToNextQuestion();
-            console.log(questionIndex)
           }, 1000);
     });
 });
 
-
+// Initializing the Test
 initTest();
